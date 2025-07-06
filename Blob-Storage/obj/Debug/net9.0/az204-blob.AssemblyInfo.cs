1fb5a51b03180b5322acde1f4ede02c7f3184812
kekaze <jroyceyanson@gmail.com>
2025-07-07 06:59:03 +0800

    add sample files for Azure Functions activity

--- Blob-Storage/obj/Debug/net9.0/az204-blob.AssemblyInfo.cs
+++ Blob-Storage/obj/Debug/net9.0/az204-blob.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9761f60227784aa1f700dd1feb416045fa13971c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a68b6d2cf74ee70de7d98e31f27a3533eebed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
